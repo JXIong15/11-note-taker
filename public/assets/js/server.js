54f225ba4813af0dc8e71cb669471a2f3f5de88e
Pikachu <jouxiong15@gmail.com>
2021-04-15 15:07:49 -0500

    added routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -8,16 +8,32 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
+var idNum = 1;
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Basic route that sends the user first to the AJAX Page
+// Basic routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+app.get('/api/notes', (req, res) => {
+    fs.readFileSync(path.join(__dirname, '../../../db/db.json'));
+    console.log("fs");
+});
 
-
+// Create New Note - takes in JSON input
+app.post('/api/notes', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newNote = req.body;
+  
+    newNote.routeName = newNote.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newNote);
+  
+    db.json.push(newNote);
+    res.json(newNote);
+  });
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
